34e172589a1beb1e352f48d5c368f26f6a01531f
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-11-20 10:07:17 +0100

    Create NotifyObject.cs

---
+++ src/Model/NotifyObject.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace LanguageEditor
+{
+    public class NotifyObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Notify(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
