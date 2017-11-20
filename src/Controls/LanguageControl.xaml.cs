544fb333b16947a3a6d6e94ce48140e8566e835c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-11-20 10:06:29 +0100

    Create LanguageControl.xaml.cs

---
+++ src/Controls/LanguageControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace LanguageEditor
+{
+    public partial class LanguageControl : UserControl
+    {
+        public LanguageControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
